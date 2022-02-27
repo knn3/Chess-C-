a1c2c5d5128b6c287871806232a3fc21d14ce12c
Nguyên Nguyễn <nguyen@d142-058-171-212.wireless.sfu.ca>
2022-02-27 15:45:59 -0800

    add Pieces

--- ChessCSharp/Board.cs
+++ ChessCSharp/Board.cs
@@ -3,8 +3,7 @@ namespace ChessCSharp
 {
     public class Board
     {
-        private int col;
-        private int row;
+        Pieces
 
         public Board()
         {
--- ChessCSharp/Pieces.cs
+++ ChessCSharp/Pieces.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace ChessCSharp
 {
     public class Pieces
     {
+        private int x, y;
+        private List<int> move;
+
         public Pieces()
         {
+            x = y = 0;
         }
+
+        public int getX() 
+        {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public void setX(int num)
+        {
+            x = num;
+        }
+        public void setY(int num)
+        {
+            y = num;
+        }
+        public List<int> getMove()
+        {
+            return move;
+        }
+        public void setMove(int numX, int numY)
+        {
+            move.Add()
+
+        }
+
     }
 }
